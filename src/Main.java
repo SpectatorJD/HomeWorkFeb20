4d8037cce342c9b47c18221780b641951d4ee94a
Dmitrtii <Tkakishakd@mail.ru>
2023-02-20 22:26:39 +0300

    HWFeb20

--- src/Main.java
+++ src/Main.java
@@ -6,11 +6,11 @@ public class Main {
 
     public static void task1 () {
         System.out.println("Задача 1");
-        ... // Пишем код для задачи 1
+         // Пишем код для задачи 1
     }
 
     public static void task2 () {
         System.out.println("Задача 2");
-        ... // Пишем код для задачи 2
+         // Пишем код для задачи 2
     }
 }
